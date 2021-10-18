c9e666d1babde05f988f9a83a6346ede5bf188dd
Luis Salas <pirrissalas_10@hotmail.com>
2021-10-17 19:02:06 -0600

    First commit, arquitecture defined

---
+++ Books/Repository/BooksRepository.py
@@ -0,0 +1,14 @@
+#Database diccionary - This is the default data for Books
+
+booksArray = {1:["Cien años de soledad", "Novela", "Gabriel García Márquez"],
+              2:["Don Quijote de la Mancha", "Aburrido", "Miguel de Cervantes"],
+              3:["Hamlet", "Teatro", "William Shakespeare"],
+              4:["El Principito", "Fantasía", "Antoine de Saint Exupéry"],
+              5:["Orgullo y Prejuicio", "Romántico", "Jane Austen"],
+              6:["1984", "Misterio", "George Orwell"],
+              7:["La Odisea", "Poesía", "Homero"],
+              8:["El retrato de Dorian Grey", "Fantasía", "Oscar Wilde"],
+              9:["Lo que el viento se llevó", "Drama", "Margaret Mitchell"],
+              10:["Moby-Dick", "Fantasía", "Herman Melville"]}
+
+books = booksArray
---
+++ Persons/Repository/PersonsRepository.py
@@ -0,0 +1,21 @@
+#Database diccionary - This is the default data for Persons
+
+personsArray = {1:["Nayarit Aguirre Aguirre", "nayari@gmail.com"],
+                2:["Antonio José Araya Ruiz", "antonio@gmail.com"],
+                3:["María Fernanda Arguedas González", "maria@gmail.com"],
+                4:["José Daniel Benavides Obando", "jose@gmail.com"],
+                5:["Carlos Andrés Berrocal Espinoza", "carlos@gmail.com"],
+                6:["Rita Irene Chang Rodriguez", "rita@gmail.com"],
+                7:["Jorge Andrés Chaves Gomez", "jorge@gmail.com"],
+                8:["Daniel Cordero Araya", "daniel@gmail.com"],
+                9:["Jonathan Andrés Flores Cedeño", "jonathan@gmail.com"],
+                10:["Wilfredo Gomez Herrera", "wilfredo@gmail.com"],
+                11:["David Gomez Zumbado", "david@gmail.com"],
+                12:["Stefany Andrea Masis Aguilar", "stefany@gmail.com"],
+                13:["Magaly Matamoros Zamora", "magaly@gmail.com"],
+                14:["Danny Mendoza González", "danny@gmail.com"],
+                15:["Ronald Fabian Meneses Montero", "ronald@gmail.com"],
+                16:["Gabriel Alberto Serrano Rosabal", "gabriel@gmail.com"],
+                17:["Víctor José Porras Herrera", "victor @gmail.com"]}
+
+persons = personsArray
